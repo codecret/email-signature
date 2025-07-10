64ab15478252e7359e0a545f59e10940eb08146a
codecret <mohammedmalek2016@gmail.com>
2025-07-10 10:38:50 +0400

    seo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import EmailSignatureGenerator from "./components/File";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Email Signature Generator",
+  description: "Generate professional email signatures with ease",
+};
 
 export default function page() {
   return (
